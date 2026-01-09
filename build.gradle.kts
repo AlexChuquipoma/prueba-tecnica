712c63982086118bc67e5d216a944f82c89581d8
AlexChuquipoma <alexrdfchuquipomavs@gmail.com>
2026-01-09 18:09:41 -0500

    prueba

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.company"
@@ -10,7 +10,7 @@ description = "Vehicle Management API"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
